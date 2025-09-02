5f7ef5bd598f05cd1226fd1600a346215af0326b
UsmanA <usmanaminbajwa@gmail.com>
2025-09-02 12:25:53 +0500

    updated password

--- utils/testSetup.js
+++ utils/testSetup.js
@@ -4,7 +4,7 @@ import { LoginPage } from '../pages/LoginPage';
 export const test = base.extend({
   page: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
-    await loginPage.login('qa.superadmin@atxtrax.com','DiGi@dm2032');
+    await loginPage.login('qa.superadmin@atxtrax.com','DiGi@dm2033');
     await use(page);
   }
 });
